2e497957b8826bcfccbff664c44778d8c6b5fe0d
namimi928 <soyongnam@gmail.com>
2016-01-26 00:15:12 -0500

    no message

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,28 @@
 def division(num1, num2)
- 
-end
+  num1 / num2
+ end
 
 def assign_variable(value)
-  
+  value 
 end
 
-def argue
-  
+def argue(phrase)
+  phrase
 end
 
-def greeting
-
+def greeting(name, age)
+ puts name 
+ puts age
 end
 
 def return_a_value
-
+  return "Nice"
 end
 
 def last_evaluated_value
-
+return "expert"
 end
 
-def pizza_party
-
+def pizza_party(yum = "cheese")
+return yum
 end
